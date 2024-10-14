4c05c90f3b5124b753639321c05b022310b2cb69
raffll28 <57024880+raffll28@users.noreply.github.com>
2024-10-14 19:44:57 +0000

    modificando pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Oi, se você conseguiu abrir isso, mem manda uma mensagem no discord falando "CHA CHA CHA" kkkkk</h1>
+    return <h1>Oi, se você conseguiu abrir isso, mem manda uma mensagem falando "CHA CHA CHA" kkkkk</h1>
 }
 
 export default Home
