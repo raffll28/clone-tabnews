78d124977daeb51df097caf652d3ec60f88a42b0
raffll28 <57024880+raffll28@users.noreply.github.com>
2024-11-06 19:07:14 +0000

    adicionando  e

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,8 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da media" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  response.status(200).json({ chave: result });
 }
 
 export default status;
