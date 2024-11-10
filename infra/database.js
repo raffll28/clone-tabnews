72bbd07a7f7d663335a14c075fd19b9d9046f561
raffll28 <57024880+raffll28@users.noreply.github.com>
2024-11-10 01:44:53 +0000

    deixando o `database.js` e `cliente.conect()` mais robustos contra erros

--- infra/database.js
+++ infra/database.js
@@ -1,15 +1,17 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  const creden = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
+  };
+  const client = new Client(creden);
+  console.log(creden);
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch {
