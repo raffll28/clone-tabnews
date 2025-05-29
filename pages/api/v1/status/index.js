a0e883f44bc74e97152fd2fbd8d6f470c8f52bb3
raffll28 <57024880+raffll28@users.noreply.github.com>
2025-05-29 15:36:28 +0000

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,33 +1,44 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/error";
 
 async function status(request, response) {
-  const updateAt = new Date().toISOString();
+  try {
+    const updateAt = new Date().toISOString();
 
-  const versionPostgresJson = await database.query("SHOW server_version;");
-  const versionPostgres = versionPostgresJson.rows[0].server_version;
+    const versionPostgresJson = await database.query("SHOW server_version;");
+    const versionPostgres = versionPostgresJson.rows[0].server_version;
 
-  const maxConnectionJson = await database.query("SHOW max_connections;");
-  const maxConnection = Number(maxConnectionJson.rows[0].max_connections);
+    const maxConnectionJson = await database.query("SHOW max_connections;");
+    const maxConnection = Number(maxConnectionJson.rows[0].max_connections);
 
-  const databaseName = process.env.POSTGRES_DB;
-  const activeConnectionsJson = await database.query({
-    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-  const activeConnections = activeConnectionsJson.rows[0].count;
+    const databaseName = process.env.POSTGRES_DB;
+    const activeConnectionsJson = await database.query({
+      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    const activeConnections = activeConnectionsJson.rows[0].count;
 
-  const map = {
-    update_at: updateAt,
-    dependencies: {
-      database: {
-        version_postgres: versionPostgres,
-        max_connections: maxConnection,
-        active_connections: activeConnections,
+    const map = {
+      update_at: updateAt,
+      dependencies: {
+        database: {
+          version_postgres: versionPostgres,
+          max_connections: maxConnection,
+          active_connections: activeConnections,
+        },
       },
-    },
-  };
-  response.status(200).json(map);
-  //console.log(map);
+    };
+    response.status(200).json(map);
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controler");
+    console.log(publicErrorObject);
+
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
