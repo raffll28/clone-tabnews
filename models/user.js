46bfbd55d9a9c41851fceee17207689ed5af733c
raffll28 <57024880+raffll28@users.noreply.github.com>
2025-09-16 20:59:10 +0000

    refactor: refactor to add sessions and feactures

--- models/user.js
+++ models/user.js
@@ -32,6 +32,36 @@ async function findOneByUsername(username) {
     return results.rows[0];
   }
 }
+async function findOneByEmail(email) {
+  const userFound = await runSelectQuery(email);
+
+  return userFound;
+
+  async function runSelectQuery(email) {
+    const results = await database.query({
+      text: `
+    SELECT 
+      *
+    FROM
+      users
+    WHERE
+      LOWER(email) = LOWER($1)
+    LIMIT
+      1
+    ;`,
+      values: [email],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O email informado não foi encontrado no sistema",
+        action: "Verifique se o email esta digitado corretamente",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validatedUniqueUsername(userInputValues.username);
@@ -166,6 +196,7 @@ const user = {
   create,
   findOneByUsername,
   update,
+  findOneByEmail,
 };
 
 export default user;
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -15,7 +15,6 @@ async function waitForAllServices() {
     });
 
     async function fetchStatusPage(bail, tryNumber) {
-      console.log(tryNumber);
       const response = await fetch("http://localhost:3000/api/v1/status");
 
       if (response.status !== 200) {
